978bb1f1f196ed059f64510762aa28691d920c73
arunavaDe <arunava.de@gmail.com>
2019-09-09 17:24:54 -0400

    Updates

---
+++ precache-manifest.cd2d3e261ef60816275e18025b8bf49f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b92f49d3b0a1a471fbf34889cb995e2",
+    "url": "/arunavaDeProjectFiveNew/index.html"
+  },
+  {
+    "revision": "35f74c76ad0a9cdd5bd7",
+    "url": "/arunavaDeProjectFiveNew/static/css/main.5e1f2c01.chunk.css"
+  },
+  {
+    "revision": "ee1401b4f9f7a41cc881",
+    "url": "/arunavaDeProjectFiveNew/static/js/2.217cbff7.chunk.js"
+  },
+  {
+    "revision": "35f74c76ad0a9cdd5bd7",
+    "url": "/arunavaDeProjectFiveNew/static/js/main.849d6905.chunk.js"
+  },
+  {
+    "revision": "3385bdef103517101f00",
+    "url": "/arunavaDeProjectFiveNew/static/js/runtime~main.bfdada46.js"
+  }
+]);
